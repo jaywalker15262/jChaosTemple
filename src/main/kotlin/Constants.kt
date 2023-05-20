5911de9f6397796fce6a8476a49054c1684a7685
Jay <jaywalkercooks@gmail.com>
2023-05-20 23:54:49 +0200

    Grouping and seperating some constants for readability and navigation

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -9,10 +9,12 @@ import org.powbot.api.rt4.World
 object Constants {
     var stopAtLvl = 99
     var emptySlotCountCheck = 27
-    var timeSinceLastXpDrop: Long = 0
     var lastKnownPrayerXp = 0
+    var timeSinceLastXpDrop: Long = 0
+
     var protectItem = true
     var depositEquipment = false
+
     var boneType = "Dragon Bones"
     val BONE_TYPES = arrayOf("Dragon bones","Lava dragon bones","Dagannoth bones","Wyvern bones",
         "Big bones", "Superior dragon bones","Bones","Hydra bones","Babydragon bones")
@@ -23,8 +25,10 @@ object Constants {
     val altarTile = Tile(2948, 3821, 0)
     val suicideTile = Tile(2982, 3848, 0)
     val suicideTileMatrix = suicideTile.matrix()
+
     val AREA_LUMBY = Area(Tile(3203, 3227, 0), Tile(3226, 3205, 0))
     val AREA_ALTAR = Area(Tile(2948, 3822, 0), Tile(2957, 3819, 0))
+
     val chaosAltarPath = TilePath(arrayOf(Tile(3028, 3842, 0), Tile(3027, 3842, 0),
         Tile(3026, 3841, 0), Tile(3025, 3840, 0), Tile(3024, 3840, 0), Tile(3023, 3839, 0),
         Tile(3022, 3839, 0), Tile(3021, 3839, 0), Tile(3020, 3839, 0),
@@ -84,11 +88,11 @@ object Constants {
 
     // antipk stuff
     var worldId = 0
+    var timeUntilNextLogOut: Long = 0
     var escapePker = false
     val loginScreenWorldHopperPoint = Point(150, 460)
     val worldSpecialtyFilter = arrayOf(World.Specialty.BOUNTY_HUNTER, World.Specialty.TARGET_WORLD,
         World.Specialty.FRESH_START, World.Specialty.HIGH_RISK, World.Specialty.BETA, World.Specialty.DEAD_MAN,
         World.Specialty.LEAGUE, World.Specialty.PVP_ARENA, World.Specialty.SKILL_REQUIREMENT,
         World.Specialty.SPEEDRUNNING, World.Specialty.TWISTED_LEAGUE)
-    var timeUntilNextLogOut: Long = 0
 }
